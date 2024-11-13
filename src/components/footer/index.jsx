5db03b8b650d0c87e07bbb8835f17fffbef15a7e
Abdulloh <tohirov080506@gmail.com>
2024-11-13 18:19:14 +0500

    third commit

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,38 +1,19 @@
 import React from 'react';
 import '../footer/style.css';
+import Logo from "../../assets/images/logo.png";
 
 const Footer = () => {
   return (
     <>
     <footer className='footer'>
-        <div className='left'>
-            <div>
-              <p>Our Company</p>
-              <a href="">History</a>
-            </div>
-            <div>
-              <p>Work With Us</p>
-              <a href="">Overview</a>
-              <a href="">See Available Positions</a>
-              <a href="">Employee Login</a>
-            </div>
-            <div>
-              <p>Shipping Solutions</p>
-              <a href="">Truckload</a>
-              <a href="">Power Only</a>
-              <a href="">Team Expedited</a>
-              <a href="">Refrigerated</a>
-            </div>
-            <div>
-              <p>Drivers</p>
-              <a href="">Company Driver</a>
-              <a href="">Owner Operator</a>
-              <a href="">Lease Program</a>
-            </div>
-        </div>
         <div className='right'>
-            <p>Copyright © 2024 Phoenix Cargo</p>
-            <a href="">Site crafted by Robintek: Columbus Website Design</a>
+            <p>Copyright © 2024 CUBICDT LLC</p>
+            <div>
+              <p>Site crafted by Robintek:</p>
+              <a href="https://hypernova.uz/">
+                <img src={Logo} alt="" />
+              </a>
+            </div>
         </div>
     </footer>
     </>
